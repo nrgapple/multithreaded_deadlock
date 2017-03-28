cfa3986da7c8a01103b905d1bb50ef3583bdaf80
nrgapple <champtoday@me.com>
2017-03-28 12:18:49 -0400

    Initial Commit

---
+++ Project 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project 2
+//
+//  Created by Adam Geiger on 3/28/17.
+//  Copyright © 2017 Adam Geiger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
